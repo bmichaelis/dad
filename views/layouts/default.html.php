af5d9536d71732ab073589fa35f4592e8076af95
Mehdi Lahmam B <mehdi@lahmam.com>
2012-08-10 16:23:23 +0200

    Add a Compass project with foundation gem. Time to rock!

--- views/layouts/default.html.php
+++ views/layouts/default.html.php
@@ -1,32 +1,47 @@
-<?php
-/**
- * Lithium: the most rad php framework
- *
- * @copyright     Copyright 2012, Union of RAD (http://union-of-rad.org)
- * @license       http://opensource.org/licenses/bsd-license.php The BSD License
- */
-?>
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 <head>
-	<?php echo $this->html->charset();?>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<title>Application &gt; <?php echo $this->title(); ?></title>
-	<?php echo $this->html->style(array('debug', 'lithium')); ?>
-	<?php echo $this->scripts(); ?>
-	<?php echo $this->html->link('Icon', null, array('type' => 'icon')); ?>
+	<meta name="viewport" content="width=device-width">
+
+	<?php
+		echo $this->html->style([
+			'debug.css',
+			'app.css',
+		]);
+
+		echo $this->html->script([
+			'modernizer-2.6.1.min.js'
+		]);
+	?>
 </head>
 <body class="app">
-	<div id="container">
-		<div id="header">
-			<h1>Application</h1>
-			<h2>
-				Powered by <?php echo $this->html->link('Lithium', 'http://lithify.me/'); ?>.
-			</h2>
-		</div>
-		<div id="content">
-			<?= $this->flashMessage->output(); ?>
-			<?php echo $this->content(); ?>
+
+	<div class="row">
+		<div class="twelve columns">
+			<h2>Welcome to Foundation</h2>
+			<p>This is version 3.0.8.rc1.</p>
+			<hr />
 		</div>
 	</div>
+
+	<div class="row">
+		<?= $this->flashMessage->output(); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $this->content(); ?>
+	</div>
+
+	<?php
+		echo $this->html->script([
+			''
+		]);
+	?>
 </body>
 </html>
--- views/projects/index.html.php
+++ views/projects/index.html.php
@@ -1,9 +1,11 @@
-<h3>Projects</h3>
+<div class="twelve columns">
+	<h3>Projects</h3>
 
-<ul id="projects">
-	<?php foreach ($projects as $project) : ?>
-	<li><?= $this->html->link($project->name, ['Projects::show', 'id' => $project->_id]) ?></li>
-	<?php endforeach; ?>
-</ul>
+	<ul id="projects">
+		<?php foreach ($projects as $project) : ?>
+		<li><?= $this->html->link($project->name, ['Projects::show', 'id' => $project->_id]) ?></li>
+		<?php endforeach; ?>
+	</ul>
 
-<?= $this->html->link('Add a project', ['Projects::add', 'http:method' => 'GET']) ?>
+	<?= $this->html->link('Add a project', ['Projects::add', 'http:method' => 'GET']) ?>
+</div>
