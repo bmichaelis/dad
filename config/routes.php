8b6b78b140d20e7c512dbff20410781b031a838b
Mehdi Lahmam B <mehdi@lahmam.com>
2012-08-11 03:10:45 +0200

    Update routes to make Projects::index the root view

--- config/routes.php
+++ config/routes.php
@@ -1,44 +1,8 @@
 <?php
 
-/**
- * The routes file is where you define your URL structure, which is an important part of the
- * [information architecture](http://en.wikipedia.org/wiki/Information_architecture) of your
- * application. Here, you can use _routes_ to match up URL pattern strings to a set of parameters,
- * usually including a controller and action to dispatch matching requests to. For more information,
- * see the `Router` and `Route` classes.
- *
- * @see lithium\net\http\Router
- * @see lithium\net\http\Route
- */
 use li3_rest\net\http\Router;
-use lithium\core\Environment;
 
-/**
- * Here, we are connecting `'/'` (the base path) to controller called `'Pages'`,
- * its action called `view()`, and we pass a param to select the view file
- * to use (in this case, `/views/pages/home.html.php`; see `app\controllers\PagesController`
- * for details).
- *
- * @see app\controllers\PagesController
- */
-Router::connect('/', 'Pages::view');
-
-/**
- * Connect the rest of `PagesController`'s URLs. This will route URLs like `/pages/about` to
- * `PagesController`, rendering `/views/pages/about.html.php` as a static page.
- */
-Router::connect('/pages/{:args}', 'Pages::view');
-
-/**
- * Add the testing routes. These routes are only connected in non-production environments, and allow
- * browser-based access to the test suite for running unit and integration tests for the Lithium
- * core, as well as your own application and any other loaded plugins or frameworks. Browse to
- * [http://path/to/app/test](/test) to run tests.
- */
-if (!Environment::is('production')) {
-	Router::connect('/test/{:args}', ['controller' => 'lithium\test\Controller']);
-	Router::connect('/test', ['controller' => 'lithium\test\Controller']);
-}
+Router::connect('/', 'Projects::index');
 
 /**
  * Handling API output formats.
@@ -56,8 +20,6 @@ Router::connect('/signin', ['Sessions::add']);
 Router::connect('/sessions', ['Sessions::create', 'http:method' => 'POST']);
 
 Router::connect('/signout', ['Sessions::delete']);
-
-
 
 /**
  * Projects routes
