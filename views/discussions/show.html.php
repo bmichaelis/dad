e8572ea719a59a580b2dfdc3858c6fb1104a454e
Mehdi Lahmam B <mehdi@lahmam.com>
2012-08-12 03:21:58 +0200

    Update Discussion and messages list main view

--- views/discussions/show.html.php
+++ views/discussions/show.html.php
@@ -6,31 +6,50 @@ $new_message_path = Router::match([
 	]);
 ?>
 
+<div class="twelve columns discussion">
 <article>
 	<header>
 		<h3><?= $discussion->subject ?></h3>
-		<p>Posted by <?= $discussion->creator->name ?> <time datetime="2012-06-16T22:31:51Z" style="">on Jun 17</time></p>
+		<p>Posted by <?= $discussion->creator->name ?> <time datetime="<?= date('c', $discussion->created_at->sec) ?>" style="">on <?= date('M j', $discussion->created_at->sec) ?></time></p>
 	</header>
 
-	<div>
-		<?= $discussion->content ?>
+	<div class="row">
+		<div class="one column creator-avatar">
+			<?= $this->html->image('http://placehold.it/80x80') ?>
+		</div>
+		<div class="eight columns end formatted-content">
+			<?php echo $discussion->content; ?>
+		</div>
+		<hr />
 	</div>
 
 	<footer>
 		<section id="messages">
 			<h4>Discuss this message</h4>
 			<?php foreach ($discussion->messages as $message) : ?>
-			<article class="message">
-				<div><strong><?= $message->creator->name ?></strong> – <?= $message->content ?></div>
+			<article class="message row">
+				<div class="one column creator-avatar">
+					<?= $this->html->image('http://placehold.it/80x80') ?>
+				</div>
+				<div class="eight columns end formatted-content">
+					<strong><?= $message->creator->name ?></strong> –
+					<?php echo $message->content; ?>
+				</div>
 			</article>
 			<?php endforeach; ?>
 		</section>
 
-		<section>
-			<?= $this->form->create(null, array('url' => $new_message_path)) ?>
-				<?= $this->form->field('content', ['type' => 'textarea', 'rows' => 2]) ?>
-				<?= $this->form->submit('Add this comment') ?>
-			<?= $this->form->end() ?>
+		<section class="row message new">
+				<div class="one column creator-avatar">
+					<?= $this->html->image('http://placehold.it/80x80') ?>
+				</div>
+				<div class="eight columns end">
+					<?= $this->form->create(null, ['url' => $new_message_path]) ?>
+						<?= $this->form->textarea('content', ['type' => 'textarea', 'rows' => 2, 'placeholder' => 'Add a massage...']) ?>
+						<?= $this->form->submit('Add this message', ['class' => 'button small radius']) ?>
+					<?= $this->form->end() ?>
+				</div>
 		</section>
 	</footer>
 </article>
+</div>
