6b9594923f5714f09e97f7cf31fd880c3ca1fc4c
Mehdi Lahmam B <mehdi@lahmam.com>
2012-08-15 19:15:15 +0200

    Hotfix elements templates locating based on a controller name. Closes #24

--- extensions/helper/Element.php
+++ extensions/helper/Element.php
@@ -6,7 +6,7 @@ class Element extends \lithium\template\Helper {
 
 	public function render($template, array $data = [], array $options = []) {
 		$data += $this->_context->data();
-		$options += ['controller' => $this->_context->request()->controller];
+		$options += ['controller' => strtolower($this->_context->request()->controller)];
 
 		return $this->_context->view()->render(['element' => $template], $data, $options);
 	}
