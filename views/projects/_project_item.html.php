cf78e5b1fde0fb17e2026362f9cd3840e6259920
Mehdi Lahmam B <mehdi@lahmam.com>
2013-01-01 23:12:33 +0100

    Add an 'Archived' label to highlight archived projects (temporary solution)

--- views/projects/_project_item.html.php
+++ views/projects/_project_item.html.php
@@ -3,6 +3,9 @@
 		<header class="project-header">
 			<h4 class="project-title">
 				<?= $this->html->link($project->name, ['Projects::show', 'id' => $project->_id]) ?>
+				<?php if ($project->archived): ?>
+					<span class="label">ARCHIVED</span>
+				<?php endif; ?>
 			</h4>
 			<p><?= $project->description ?></p>
 			<p><?= $this->text->pluralize($project->count_discussions(), 'discussion') ?></p>
